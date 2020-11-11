538ae3ae8d4563130cbd64971f97696bee4ba05e
saulide <ich@sauli.de>
2020-11-11 14:51:49 +0100

    Added oAuth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthConfig, OAuthService } from "angular-oauth2-oidc";
+
+const authConfig: AuthConfig = {
+  issuer: "https://gitlab.com",
+  clientId: "bd488d42573ef10f465e96f18df01b6895046721bb6c3cb5be86d5817fd2f2b5",
+  redirectUri: window.location.origin,
+  responseType: "code",
+  scope: "profile email api read_repository",
+  showDebugInformation: true
+};
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = "openid";
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  onLogin = () => {
+    this.oauthService.initCodeFlow();
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { OAuthModule } from "angular-oauth2-oidc";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ["https://gitlab.com/api"],
+        sendAccessToken: true
+      }
+    })
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
